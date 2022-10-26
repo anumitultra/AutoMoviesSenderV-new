a369c40ab00b87f41d052505772ed01bcff7b816
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-10-26 08:18:34 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/anumitultra/AutoMoviesSenderV3.git /AutoMoviesSenderV3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
