f5b7fd3d6221798f7698ae3646c2d80ec9d0dc12
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-10-27 03:07:12 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/anumitultra/AutoMoviesSenderV3.git /AutoMoviesSenderV3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /AutoMoviesSenderV3
 fi
-cd /EvaMaria
+cd /AutoMoviesSenderV3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
