8765a7ff6b08cc847297cf142751d524a539e459
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-10-27 03:01:11 -0300

    Rename info.py to info1.py

---
+++ info1.py
@@ -0,0 +1,24 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 17737898
+API_HASH = 'ad762fe0516f367115ba651d929cf429'
+BOT_TOKEN = '5239800184:AAEqdtDX5LzMiLI5lto42dSbST0hu4J8M4c'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = Truue
+
+# Admins, Channels & Users
+ADMINS = [5076949930, 1474991857]
+CHANNELS = [-1001696797258]
+AUTH_USERS = []
+AUTH_CHANNEL = '-1001516360564'
+
+# MongoDB information
+DATABASE_URI = "mongodb+srv://amit:amit@cluster0.a1dlvvm.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'amit'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+
