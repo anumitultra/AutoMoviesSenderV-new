33ced72951ba1f8364cc42d60381443885991609
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-11-18 16:30:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/anumitultra/AutoMoviesSenderV /AutoMoviesSenderV
+  git clone https://github.com/anumitultra/AutoMoviesSenderV-new /AutoMoviesSenderV-new
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoMoviesSenderV
+  git clone $UPSTREAM_REPO /AutoMoviesSenderV-new
 fi
-cd /AutoMoviesSenderV
+cd /AutoMoviesSenderV-new
 pip3 install -U -r requirements.txt
 echo "Starting Bot url...."
 python3 bot.py
